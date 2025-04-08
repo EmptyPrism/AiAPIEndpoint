512c9ac3c1da630d9a44e1612badabf652cc7628
EmptyPrism <polinskijulian10@gmail.com>
2025-04-08 16:32:51 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "aiweaponendpoint",
+  "version": "1.0.0",
+  "description": "AI-based weapon recommendation endpoint",
+  "main": "index.js",
+  "dependencies": {
+    "axios": "^1.4.0",
+    "express": "^4.18.2"
+  },
+  "scripts": {
+    "start": "node index.js"
+  },
+  "license": "MIT"
+}
